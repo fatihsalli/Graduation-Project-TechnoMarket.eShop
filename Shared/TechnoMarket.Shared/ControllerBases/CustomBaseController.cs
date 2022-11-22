3fcd7e91aa2798bf79db3e181633558cf89d13e1
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-22 20:12:13 +0300

    .

---
+++ Shared/TechnoMarket.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnoMarket.Shared.Dtos;
+
+namespace TechnoMarket.Shared.ControllerBases
+{
+    public class CustomBaseController:ControllerBase
+    {
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode==204)
+            {
+                return new ObjectResult(null) 
+                { 
+                    StatusCode = response.StatusCode 
+                };
+            }
+
+            return new ObjectResult(response)
+            {
+                StatusCode=response.StatusCode,
+            };
+        }
+
+
+
+
+    }
+}
---
+++ Shared/TechnoMarket.Shared/Dtos/CustomResponseDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TechnoMarket.Shared.Dtos
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; set; }
+
+        public List<string> Errors { get; set; }
+
+        //Static Factory Metotlar
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Data = data,IsSuccessful=true };
+        }
+
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors, IsSuccessful = false };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error },IsSuccessful=false };
+        }
+
+
+
+    }
+}
