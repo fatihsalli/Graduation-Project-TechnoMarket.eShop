3356f14e5755ebbb78a3e8b43e25b3363ddab2aa
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-23 13:23:29 +0300

    Catalog.Api-Entities

---
+++ Services/Catalog/TechnoMarket.Services.Catalog/Models/Feature.cs
@@ -0,0 +1,10 @@
+﻿namespace TechnoMarket.Services.Catalog.Models
+{
+    public class Feature
+    {
+        public string Color { get; set; }
+        public string Summary { get; set; }
+
+
+    }
+}
---
+++ Services/Catalog/TechnoMarket.Services.Catalog/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TechnoMarket.Services.Catalog.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CustomerId { get; set; }
+        public string ImageFile { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+
+        //Embedded Document Pattern
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        //Mongo.Db tarafında collectionlara yansıtırken göz ardı edip program içerisinde kendimiz kullanmak için tanımladık.
+        [BsonIgnore]
+        public Category Category { get; set; }
+
+
+
+    }
+}
