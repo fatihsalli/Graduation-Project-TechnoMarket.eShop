f0c203238dd107329010b230b20a002831e5b0e7
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-27 22:44:44 +0300

    ICategoryService

--- Services/Catalog/TechnoMarket.Services.Catalog/Services/Interfaces/ICategoryService.cs
+++ Services/Catalog/TechnoMarket.Services.Catalog/Services/Interfaces/ICategoryService.cs
@@ -5,8 +5,10 @@ namespace TechnoMarket.Services.Catalog.Services.Interfaces
 {
     public interface ICategoryService
     {
-        public Task<CustomResponseDto<CategoryDto>> CreateAsync(CategoryCreateDto categoryCreateDto);
         public Task<CustomResponseDto<List<CategoryDto>>> GetAllAsync();
-
+        public Task<CustomResponseDto<CategoryDto>> GetById(string id);
+        public Task<CustomResponseDto<CategoryDto>> CreateAsync(CategoryCreateDto categoryCreateDto);
+        public Task<CustomResponseDto<CategoryDto>> UpdateAsync(CategoryUpdateDto categoryUpdateDto);
+        public Task<CustomResponseDto<NoContentDto>> DeleteAsync(string id);
     }
 }
