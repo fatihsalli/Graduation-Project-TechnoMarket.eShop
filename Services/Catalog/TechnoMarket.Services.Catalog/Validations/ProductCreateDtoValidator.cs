194fe545c94b33631204f2229db5d1da28b64d1e
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-30 15:19:18 +0300

    ProductCreateDtoValidator

---
+++ Services/Catalog/TechnoMarket.Services.Catalog/Validations/ProductCreateDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TechnoMarket.Services.Catalog.Dtos;
+
+namespace TechnoMarket.Services.Catalog.Validations
+{
+    public class ProductCreateDtoValidator:AbstractValidator<ProductCreateDto>
+    {
+        public ProductCreateDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.Price).InclusiveBetween(1, decimal.MaxValue).WithMessage("{PropertyName} must be greater 0");
+        }
+    }
+}
