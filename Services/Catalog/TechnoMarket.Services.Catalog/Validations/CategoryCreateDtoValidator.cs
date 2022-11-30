66709c6ce162f939fa1013655cd939fec96f044e
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-30 15:22:38 +0300

    CategoryCreateDtoValidator

---
+++ Services/Catalog/TechnoMarket.Services.Catalog/Validations/CategoryCreateDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TechnoMarket.Services.Catalog.Dtos;
+
+namespace TechnoMarket.Services.Catalog.Validations
+{
+    public class CategoryCreateDtoValidator:AbstractValidator<CategoryCreateDto>
+    {
+        public CategoryCreateDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+        }
+    }
+}
