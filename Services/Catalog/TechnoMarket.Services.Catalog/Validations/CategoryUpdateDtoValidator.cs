e4900dd028ba2a5cd74779763782f4364fd5e66a
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-30 15:24:22 +0300

    CategoryUpdateDtoValidator

---
+++ Services/Catalog/TechnoMarket.Services.Catalog/Validations/CategoryUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using TechnoMarket.Services.Catalog.Dtos;
+
+namespace TechnoMarket.Services.Catalog.Validations
+{
+    public class CategoryUpdateDtoValidator:AbstractValidator<CategoryUpdateDto>
+    {
+        public CategoryUpdateDtoValidator()
+        {
+            RuleFor(x => x.Id).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required").Length(24).WithMessage("{PropertyName} must be 24 character");
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+        }
+    }
+}
