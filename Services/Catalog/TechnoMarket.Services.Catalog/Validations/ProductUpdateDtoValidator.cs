776ff2af16d44aa7953a2ef45bc0eacef8141bea
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-30 14:54:48 +0300

    ProductUpdateDtoValidator

--- Services/Catalog/TechnoMarket.Services.Catalog/Validations/ProductUpdateDtoValidator.cs
+++ Services/Catalog/TechnoMarket.Services.Catalog/Validations/ProductUpdateDtoValidator.cs
@@ -7,11 +7,10 @@ namespace TechnoMarket.Services.Catalog.Validations
     {
         public ProductUpdateDtoValidator()
         {
+            RuleFor(x => x.Id).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required").Length(24).WithMessage("{PropertyName} must be 24 character");
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
-
-
-
-
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.Price).InclusiveBetween(1, decimal.MaxValue).WithMessage("{PropertyName} must be greater 0");
         }
     }
 }
