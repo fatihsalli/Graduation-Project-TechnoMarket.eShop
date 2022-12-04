d19019054df37eceebfcbec251a27d41f1ddb356
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-04 13:09:53 +0300

    GetById_IdValid_ReturnSuccessResult

--- Services/Catalog/TechnoMarket.Services.Catalog.UnitTests/ProductsControllerTest.cs
+++ Services/Catalog/TechnoMarket.Services.Catalog.UnitTests/ProductsControllerTest.cs
@@ -30,8 +30,8 @@ namespace TechnoMarket.Services.Catalog.UnitTests
             _mockProductService = new Mock<IProductService>();
             _productsController = new ProductsController(_mockProductService.Object);
 
-            _products= new List<ProductDto>() 
-            {                
+            _products = new List<ProductDto>()
+            {
                 new ProductDto()
                 {
                     Id="507f191e810c19729de860ea",
@@ -49,16 +49,16 @@ namespace TechnoMarket.Services.Catalog.UnitTests
                     Price=3500.00M,
                     Description="ZenBook Pro Duo 15 OLED lets you get things done in style: calmly, efficiently, and with zero fuss. It’s your powerful and elegant next-level companion for on-the-go productivity and creativity, featuring an amazing 4K OLED HDR touchscreen.",
                     ProductFeature="Color=black Height=9.81 inches Width=14.17 inches 2.34 kg"
-                } 
+                }
             };
         }
 
         [Fact]
-        public async void GetAll_ActionExecute_ReturnOkResult()
+        public async void GetAll_ActionExecute_ReturnSuccessResult()
         {
-            _mockProductService.Setup(x => x.GetAllAsync()).ReturnsAsync(CustomResponseDto<List<ProductDto>>.Success(200,_products));
+            _mockProductService.Setup(x => x.GetAllAsync()).ReturnsAsync(CustomResponseDto<List<ProductDto>>.Success(200, _products));
 
-            var result =await _productsController.GetAll();
+            var result = await _productsController.GetAll();
 
             var createActionResult = Assert.IsType<ObjectResult>(result);
 
@@ -70,12 +70,37 @@ namespace TechnoMarket.Services.Catalog.UnitTests
         }
 
         [Theory]
+        [InlineData("507f191e810c19729de860ea")]
+        [InlineData("511f191e810c19729de860fr")]
+        public async void GetById_IdValid_ReturnSuccessResult(string id)
+        {
+            var product = _products.First(x => x.Id == id);
+
+            _mockProductService.Setup(x => x.GetByIdAsync(id)).ReturnsAsync(CustomResponseDto<ProductDto>.Success(200, product));
+
+            var result = await _productsController.GetById(id);
+
+            var createActionResult = Assert.IsType<ObjectResult>(result);
+
+            Assert.Equal(200, createActionResult.StatusCode);
+
+            var returnProducts = Assert.IsAssignableFrom<CustomResponseDto<ProductDto>>(createActionResult.Value);
+
+            Assert.Equal(id, returnProducts.Data.Id);
+            Assert.Equal(product.Name, returnProducts.Data.Name);
+            Assert.Equal(product.Stock, returnProducts.Data.Stock);
+            Assert.Equal(product.Price, returnProducts.Data.Price);
+            Assert.Equal(product.Description, returnProducts.Data.Description);
+            Assert.Equal(product.ProductFeature, returnProducts.Data.ProductFeature);
+        }
+
+        [Theory]
         [InlineData("501f191e810c19729de860ea")]
-        public async void GetById_IdNotFound_ReturnNotFoundException(string id)
+        public async void GetById_IdInValid_ReturnNotFoundException(string id)
         {
             _mockProductService.Setup(x => x.GetByIdAsync(id)).Throws(new NotFoundException($"Product ({id}) not found!"));
 
-            Exception exception=await Assert.ThrowsAsync<NotFoundException>(()=> _productsController.GetById(id));
+            Exception exception = await Assert.ThrowsAsync<NotFoundException>(() => _productsController.GetById(id));
 
             Assert.IsType<NotFoundException>(exception);
 
