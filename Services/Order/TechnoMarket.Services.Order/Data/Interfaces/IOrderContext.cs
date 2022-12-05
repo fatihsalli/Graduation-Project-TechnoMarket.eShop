9f07149a542de5881e8544e980cbac821eca0368
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-05 15:44:05 +0300

    OrderContext

---
+++ Services/Order/TechnoMarket.Services.Order/Data/Interfaces/IOrderContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace TechnoMarket.Services.Order.Data.Interfaces
+{
+    public interface IOrderContext
+    {
+        IMongoCollection<Models.Order> Orders { get; }
+    }
+}
---
+++ Services/Order/TechnoMarket.Services.Order/Data/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using TechnoMarket.Services.Order.Data.Interfaces;
+using TechnoMarket.Services.Order.Models;
+using TechnoMarket.Services.Order.Settings.Interfaces;
+
+namespace TechnoMarket.Services.Order.Data
+{
+    public class OrderContext : IOrderContext
+    {
+        public OrderContext(IOrderDatabaseSettings orderDatabaseSettings)
+        {
+            var client= new MongoClient(orderDatabaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(orderDatabaseSettings.DatabaseName);
+
+            Orders = database.GetCollection<Models.Order>(orderDatabaseSettings.OrderCollectionName);
+
+            //Seed Data yazılacak
+        }
+
+        public IMongoCollection<Models.Order> Orders { get; }
+    }
+}
