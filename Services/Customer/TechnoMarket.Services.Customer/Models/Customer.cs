129f42ac59df7af383995914abf74a37625c1ece
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-07 16:44:34 +0300

    Models

---
+++ Services/Customer/TechnoMarket.Services.Customer/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace TechnoMarket.Services.Customer.Models
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
