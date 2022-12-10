3d2993fe0ac9936801564a8dc54800d57c167c96
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-10 14:21:33 +0300

    .

--- Services/Customer/TechnoMarket.Services.Customer/Migrations/20221210112040_mig01.Designer.cs
+++ Services/Customer/TechnoMarket.Services.Customer/Migrations/20221210112040_mig01.Designer.cs
@@ -12,7 +12,7 @@ using TechnoMarket.Services.Customer.Data;
 namespace TechnoMarket.Services.Customer.Migrations
 {
     [DbContext(typeof(CustomerDbContext))]
-    [Migration("20221210092749_mig01")]
+    [Migration("20221210112040_mig01")]
     partial class mig01
     {
         /// <inheritdoc />
