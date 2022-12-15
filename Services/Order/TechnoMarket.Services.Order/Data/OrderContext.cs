6fc385f53cfbeb0d9edcd369a07fa7116946dc18
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-15 14:33:25 +0300

    changing-for-uuid

--- Services/Order/TechnoMarket.Services.Order/Data/OrderContext.cs
+++ Services/Order/TechnoMarket.Services.Order/Data/OrderContext.cs
@@ -11,18 +11,18 @@ namespace TechnoMarket.Services.Order.Data
     {
         public OrderContext(IOrderDatabaseSettings orderDatabaseSettings)
         {
-            //To create UUID
-            //BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+            #region Neden CollectionSettings Düzenledik?
+            //Önemli !!! GuidRepresentation.Standard yaptığımız durumda find metodunda bulamıyordu. GuidRepresentation.CSharpLegacy yapmamızı bekliyordu. O sebeple bu ayarlamayı yaptık. Global anlamda bunu yapmak için "Global.asax.cs" dosyası oluşturarak yapmamız gerekiyordu. Onun yerine aşağıda tanımladık. Sorunsuz çalışıyor. 
+            #endregion
+            var collectionSettings = new MongoCollectionSettings { GuidRepresentation = GuidRepresentation.Standard };
 
             var client = new MongoClient(orderDatabaseSettings.ConnectionString);
 
             var database = client.GetDatabase(orderDatabaseSettings.DatabaseName);
 
-            Orders = database.GetCollection<Models.Order>(orderDatabaseSettings.OrderCollectionName);
-
+            Orders = database.GetCollection<Models.Order>(orderDatabaseSettings.OrderCollectionName,collectionSettings);
 
             //OrderContextSeed.SeedData(Orders);
-
         }
 
         public IMongoCollection<Models.Order> Orders { get; }
