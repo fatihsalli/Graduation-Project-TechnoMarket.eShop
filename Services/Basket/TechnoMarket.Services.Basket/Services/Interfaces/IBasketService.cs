e4e5068f4d9d6734790c433610bfd2fffedba770
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-16 10:50:52 +0300

    IBasketService

--- Services/Basket/TechnoMarket.Services.Basket/Services/Interfaces/IBasketService.cs
+++ Services/Basket/TechnoMarket.Services.Basket/Services/Interfaces/IBasketService.cs
@@ -1,6 +1,12 @@
-﻿namespace TechnoMarket.Services.Basket.Services.Interfaces
+﻿using TechnoMarket.Services.Basket.Dtos;
+
+namespace TechnoMarket.Services.Basket.Services.Interfaces
 {
     public interface IBasketService
     {
+        Task<BasketDto> GetBasket(string customerId);
+        Task<bool> SaveOrUpdate(BasketDto basketDto);
+        Task<bool> Delete(string customerId);
+
     }
 }
