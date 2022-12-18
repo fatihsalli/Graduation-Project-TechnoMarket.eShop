f9cf3bc06e5941e91ae5111c67d1f734706d836a
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-18 15:46:12 +0300

    gateway-ocelot

--- Gateways/TechnoMarket.Gateway/Program.cs
+++ Gateways/TechnoMarket.Gateway/Program.cs
@@ -1,25 +1,29 @@
+using Microsoft.Extensions.Hosting.Internal;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region .Net6 Kullanýmý-Ocelot
+builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
+
 //Ocelot kütüphanesini ekledik.
-builder.Services.AddOcelot();
+builder.Services.AddOcelot(builder.Configuration); 
+#endregion
 
-
+#region .Net5 Kullanýmý-Ocelot
+//=> environment'a göre configuration dosyasýný verdik Ocelot için.
+//Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingcontext, config) =>
+//{
+//    config.AddJsonFile($"configuration.{hostingcontext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
+//})
+//    .ConfigureWebHostDefaults(webBuilder =>
+//    {
+//        webBuilder.UseStartup<Program>();
+//    }); 
+#endregion
 
 var app = builder.Build();
-
-//=> environment'a göre configuration dosyasýný verdik Ocelot için.
-Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingcontext, config) =>
-{
-    config.AddJsonFile($"configuration.{hostingcontext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
-})
-    .ConfigureWebHostDefaults(webBuilder=>
-    {
-        webBuilder.UseStartup<Program>();
-    });
-
 
 if (app.Environment.IsDevelopment())
 {
