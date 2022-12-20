00e792d6e6abfe83e056131567766085c00bb5bd
Fatih Şallı <mimsallifatih@gmail.com>
2022-12-20 14:58:51 +0300

    ProductController-MVC

---
+++ Frontends/TechnoMarket.Web/Areas/Admin/Models/ProductCreateInput.cs
@@ -0,0 +1,13 @@
+﻿namespace TechnoMarket.Web.Areas.Admin.Models
+{
+    public class ProductCreateInput
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public ProductFeatureCreateInput Feature { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ Frontends/TechnoMarket.Web/Areas/Admin/Models/ProductFeatureCreateInput.cs
@@ -0,0 +1,10 @@
+﻿namespace TechnoMarket.Web.Areas.Admin.Models
+{
+    public class ProductFeatureCreateInput
+    {
+        public string Color { get; set; }
+        public string Height { get; set; }
+        public string Width { get; set; }
+        public string Weight { get; set; }
+    }
+}
