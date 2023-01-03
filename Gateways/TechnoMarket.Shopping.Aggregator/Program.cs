1044be65e98fc78a7fc71b73c8f370d94f5d59a2
Fatih Şallı <mimsallifatih@gmail.com>
2023-01-03 14:37:29 +0300

    .

--- Gateways/TechnoMarket.Shopping.Aggregator/Program.cs
+++ Gateways/TechnoMarket.Shopping.Aggregator/Program.cs
@@ -1,23 +1,35 @@
+using Serilog;
+using TechnoMarket.Shared.CommonLogging;
 using TechnoMarket.Shopping.Aggregator.Services;
 using TechnoMarket.Shopping.Aggregator.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//=> Shared üzerinden ulaþarak gerekli düzenlemeleri yapýyoruz. Oldukça temiz bir yaklaþým.
+builder.Host.UseSerilog(SeriLogger.Configure);
+
+//=> Loglamamýmýz handle etmek için oluþturduðumuz classýmýzý ekliyoruz.
+builder.Services.AddTransient<LoggingDelegatingHandler>();
+
 //HttpClient => Catalog
 builder.Services.AddHttpClient<ICustomerService, CustomerService>(c =>
-                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CustomerUrl"]));
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CustomerUrl"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 //HttpClient => Order
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
-                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderUrl"]));
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderUrl"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 //HttpClient => Basket
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
-                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]));
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
